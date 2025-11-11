735d1a7adf3bdb72d1eef54e332e2d800b7e77dc
luis-guico <ldguico@gmail.com>
2025-11-11 21:42:22 +0800

    dice game update
    new update

--- dice_roller_game.py
+++ dice_roller_game.py
@@ -2,22 +2,13 @@ import random
 while True:
     choice = input('Roll the dice? (y/n): ').lower()
     if choice == 'y':
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
-        print(f'({dice1},{dice2})')
+        dice_number = int(input('Enter number of dice to roll (1 - 5): '))
+        for _ in range(dice_number):
+            roll = random.randint(1, 6)
+            print(f'You rolled a {roll}')
     elif choice == 'n':
         print('Thank you for playing!')
         break
     else:
         print('Invalid choice!')
 
-    # Ask: roll the dice?
-    # if user enters y
-    #   generate 2 random numbers between 1 and 6
-    #   print the numbers
-    #if user enters n
-    #   print thank you
-    #   terminate
-    #Else
-    #   print invalid input
-    
